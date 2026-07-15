d9222c41d4f397838d8a9427738daa1f5b2dcfda
Nishant Singh <singhnishant414243@gmail.com>
2026-07-15 10:04:35 +0530

    add graph.py

---
+++ graph.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x:np.ndarray = np.arange(1,11)
+y:np.ndarray = np.random.randint(100, 500, 10)
+
+plt.figure(figsize=(5,4))
+plt.title("Bar Plot", fontdict=dict(size=15, color='gray'), loc='left')
+plt.bar(x, y, color='red')
+plt.gca().spines['top'].set_visible(False)
+plt.gca().spines['right'].set_visible(False)
+plt.gca().spines['left'].set_color('gray')
+plt.gca().spines['bottom'].set_color('gray')
+plt.xticks(color='gray')
+plt.yticks(color='gray')
+plt.tick_params(color='gray')
+plt.show()
